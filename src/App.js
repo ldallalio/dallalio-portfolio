1c63c72d812865b32cbba269908f9f1b0dc30b17
Logan Dallalio <logan@dallalioweb.dev>
2022-06-29 05:01:20 -0500

    Added Contact Page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import "./App.css";
 import React from "react";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 
 function App() {
   return (
     <>
       <Home />
       <Projects />
+      <Contact />
+      <About />
     </>
   );
 }
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from "../components/Form";
+
+function Contact() {
+  return (
+    <div className="container" id="contact">
+      <h1>Contact</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default Contact;
