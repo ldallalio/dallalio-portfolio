6fc37137f67162e0ca03d1bbadd50d151e259b5d
Logan Dallalio <logan@dallalioweb.dev>
2022-07-09 04:54:13 -0500

    updated about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,12 +5,11 @@ function About() {
     <div className="aboutContainer" id="about">
       <h1>About Me</h1>
       <p className="aboutWords">
-        My name is Logan Dallalio and I am a self-taught software engineer and I
-        am passionate about creating applications for businesses and clients to
-        use. I enjoy creating web applications using Javascript and ReactJs. I
-        live in Baton Rouge, LA and I am married for my beautiful wife of 5
-        years. We have two kids Oliver and Milo. I look forward to working with
-        you!
+        My name is Logan Dallalio, I am a self-taught software engineer. I am
+        passionate about creating applications for businesses and clients to
+        use. I enjoy using Javascript and ReactJs to create web applications. I
+        live in Baton Rouge, LA with my beautiful wife, Shelbie, and our two
+        little boys, Oliver and Milo. I look forward to working with you!
       </p>
     </div>
   );
